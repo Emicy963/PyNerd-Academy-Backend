0a2eb604c21df7e2f31683b18fb8e8b428681349
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-16 01:19:47 +0100

    feat: Create the model for manager custom user

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    """Model for Manager Custom User"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("role", "ADMIN")
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
