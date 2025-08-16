5685ecbe83bc7a882364f9b1d3d926a4a85afdc3
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-16 13:48:56 +0100

    feat: Create serializer for Module

--- apps/courses/serializers.py
+++ apps/courses/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Lesson
+from .models import Lesson, Module
 
 
 
@@ -7,3 +7,10 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = ["id", "title", "video_url", "duration_seconds"]
+
+class ModuleSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Module
+        fields = ["id", "title", "order", "lessons"]
