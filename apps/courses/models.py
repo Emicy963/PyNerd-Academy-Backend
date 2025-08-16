c424c270902841eb4deae84237b2ad14116c50b3
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-16 13:16:15 +0100

    feat: Create the model for course

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructor = models.ForeignKey(
+        "accounts.CustomUser",
+        limit_choices_to={"role": "INSTRUCTOR"},
+        on_delete=models.CASCADE,
+        related_name="courses"
+    )
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if self.instructor.role != "INSTRUCTOR":
+            raise ValidationError("Only instructor can create courses.")
+    
+    def __str__(self):
+        return self.title
