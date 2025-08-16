cf0ca6835ef240ee7ca89551672324f0cc392b85
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-16 01:52:08 +0100

    feat: Create serializer for user Profile

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, UserProfile
 
 
 
@@ -11,4 +11,14 @@ class UserSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = [
             "id", "is_approved", "created_at"
+        ]
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = [
+            "bio", "avatar", "website", "location", "birth_date"
+        ]
+        read_only_fields = [
+            "created_at", "updated_at"
         ]
