b24cd7606966608891178e528c805375fb0ad791
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-16 14:09:51 +0100

    feat: Config permissions to allows access only to students enroled in the course

--- apps/courses/permissions.py
+++ apps/courses/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from .models import Enrollment
 
 
 class IsInstructor(BasePermission):
@@ -7,4 +8,16 @@ class IsInstructor(BasePermission):
     """
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'INSTRUCTOR'
+
+class IsCourseStudent(BasePermission):
+    """
+    Allows access only to students enrolled in the course.
+    """
+    def has_object_permission(self, request, view, obj):
+        # obj is expected to be a Lesson or reated to a Course
+        course = getattr(obj, "course", None) or getattr(obj, "module", None).course
+        return request.user.role == "STUDENT" and Enrollment.objects.filter(
+            student=request.user,
+            course=course
+        ).exists()
 
