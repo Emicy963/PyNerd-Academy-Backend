f4425d0cc8102c6606ccf801350fc8594da74ee0
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-17 14:07:18 +0100

    feat: Create the Model View Set for Course View.

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,5 +1,24 @@
-from rest_framework import pagination
+from rest_framework import pagination, viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Course
+from .permissions import IsInstructor
+from .serializers import CourseSerializer
 
 class StandardResultSetPagination(pagination.LimitOffsetPagination):
     default_limit = 20
     max_limit = 50
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultSetPagination
+
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [AllowAny]
+        elif self.action in ["create", "update", "partial_update"]:
+            permission_classes = [IsAuthenticated, IsInstructor]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
