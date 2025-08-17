6e7ada0fde236e88878817a9a7b572ae9ce66f9b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-17 14:58:11 +0100

    feat: Add router in urls

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import StudentProgressView
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import StudentProgressView, CourseViewSet, ProgressViewSet
+
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet, basename="course")
+router.register(r"progress", ProgressViewSet, basename="progress")
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("students/<int:id>/progress/", StudentProgressView.as_view(), name="student-progress"),
 ]
 
