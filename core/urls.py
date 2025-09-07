4ff9564dca60f036eefdc6655d85faa89cd9c089
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-07 23:51:06 +0100

    fix(login): Fix login error

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenBlacklistView, TokenRefreshSlidingView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshSlidingView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.accounts.urls")),
     path("api/", include("apps.courses.urls")),
-    path("api/auth/login/", TokenBlacklistView.as_view(), name="token_obtain_pair"),
+    path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/auth/refresh/", TokenRefreshSlidingView.as_view(), name="token_refresh"),
 ]
