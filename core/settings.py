bf06e3e5dd716f49d2168c17a34140105cc7f4d5
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-12-22 01:28:08 +0100

    .

--- core/settings.py
+++ core/settings.py
@@ -242,7 +242,7 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     "TITLE": "PyNerd API",
     "DESCRIPTION": "Backend API for PyNerd EdTech Platform",
-    "VERSION": "1.2.0",
+    "VERSION": "1.2.1",
     "SERVE_INCLUDE_SCHEMA": False,
 }
 
